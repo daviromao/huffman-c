b4c22a87475292b0c188e029a02af8a6296871bc
daviromao <davi.romao0210@gmail.com>
2023-04-21 15:22:13 -0300

    refactor: move binarytree logic to another file

--- unzip.c
+++ unzip.c
@@ -1,56 +1,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
-typedef struct Bt Bt;
-
-struct Bt
-{
-    void *item;
-    Bt *left;
-    Bt *right;
-};
-
-Bt *create_bt(void *item, Bt *left, Bt *right)
-{
-    Bt *new_bt = (Bt *) malloc(sizeof(Bt));
-    new_bt->item = item;
-    new_bt->left = left;
-    new_bt->right = right;
-
-    return new_bt;
-}
-
-int isBitSet(unsigned char c, int i)
-{
-    unsigned char mask = 1 << i;
-    return mask & c;
-}
-
-Bt *recreateBt(unsigned char tree[], int *index)
-{
-    int item = tree[*index];
-    Bt *new_bt;
-
-    if(item=='\\')
-    {
-        new_bt = create_bt((void*)(tree+(*index)+1), NULL, NULL);
-        *index+=2;
-    }
-    else if(item=='*')
-    {
-        new_bt = create_bt(NULL, NULL, NULL);
-        *index+=1;
-        new_bt->left = recreateBt(tree, index);
-        new_bt->right = recreateBt(tree, index);
-    }
-    else
-    {
-        new_bt = create_bt((void*)(tree+(*index)), NULL, NULL);
-        *index+=1;
-    }
-    return new_bt;
-}
+#include "libs/binarytree.c"
+#include "libs/utils.c"
 
 long getTotalSize(FILE *f)
 {
@@ -74,15 +26,15 @@ void calculateTreeAndTrashSize(FILE *file, int *treeSize, int *trashSize)
     *treeSize = ((firstByte & mask) << 8) | secondByte;
 }
 
-void unzipFile(FILE *zipedFile, Bt *treeHead, long totalSize, int treeSize, int trashSize)
+void unzipFile(FILE *zipedFile, BinaryTree *treeHead, long totalSize, int treeSize, int trashSize)
 {
     int headerSize = 2 + treeSize;
     long compressedSize = totalSize - headerSize;
 
-    char *unzipedFilename = "hello.txt";
+    char *unzipedFilename = "output.extension";
     FILE *unzipped = fopen(unzipedFilename, "wb");
 
-    Bt *indexBt = treeHead;
+    BinaryTree *indexBt = treeHead;
     for(long i = 0; i < compressedSize; i++)
     {
         unsigned char byte;
@@ -122,7 +74,7 @@ int main()
     fread(preOrderTree, sizeof(unsigned char), treeSize, zippedFile);
     
     int preOrderIndex = 0;
-    Bt *tree = recreateBt(preOrderTree, &preOrderIndex);
+    BinaryTree *tree = recreateTreeFromPreorder(preOrderTree, &preOrderIndex);
 
     unzipFile(zippedFile, tree, totalSize, treeSize, trashSize);
 
